b50e5d381ffe2834cb7a6f69d32118fbb3786673
H0RIS <H0RIS@outlook.com>
2024-04-11 12:28:29 +0200

    Header files

--- cpp/TheCherno/HelloWorld/HelloWorld/Math.cpp
+++ cpp/TheCherno/HelloWorld/HelloWorld/Math.cpp
@@ -1,9 +1,29 @@
 #include "Log.h"
 
+int Add(int a, int b)
+{
+	Log("Add");
+
+	return (a + b);
+}
+
+int Substract(int a, int b)
+{
+	Log("Substract");
+
+	return (a - b);
+}
+
 int Multiply(int a, int b)
 {
 	Log("Multiply");
-	int result = a * b;
 
-	return result;
+	return (a * b);
+}
+
+double Divide(double a, double b)
+{
+	Log("Divide");
+
+	return (a / b);
 }
