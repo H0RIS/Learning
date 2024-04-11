76a49cc801f1a8f5e2066419b889ce68da7967de
H0RIS <H0RIS@outlook.com>
2024-04-11 11:24:26 +0200

    variables

--- cpp/TheCherno/HelloWorld/HelloWorld/Main.cpp
+++ cpp/TheCherno/HelloWorld/HelloWorld/Main.cpp
@@ -5,8 +5,16 @@ int Multiply(int a, int b);
 
 int main()
 {
-	std::cout << Multiply(5, 10) << std::endl;
+	unsigned int variable = 4;
+	std::cout << variable << std::endl;
 
+	variable = 42;
+	std::cout << variable << std::endl;
+
+	char a = 65;
+	std::cout << a << std::endl;
+
+	std::cout << sizeof(bool) << std::endl;
 	std::cin.get();
 
 	return 0;
