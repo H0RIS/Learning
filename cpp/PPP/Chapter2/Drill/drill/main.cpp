e90b2a42b7a7a6e1f1f1224c7d9b94e3018c4616
H0RIS <H0RIS@outlook.com>
2024-05-02 22:55:07 +0200

    Chapter2 Drill[4]

--- cpp/PPP/Chapter2/Drill/drill/main.cpp
+++ cpp/PPP/Chapter2/Drill/drill/main.cpp
@@ -1,19 +1,26 @@
-import std;
+#include "PPP.h";
 
 int main()
 {
 	std::cout << "Enter the name of the person you want to write to: \n";
 
-	std::string first_name;
+	string first_name;
 	std::cin >> first_name;
 
 	std::cout << "Dear " << first_name << ",\n";
 	std::cout << "  How are you?\nWe haven't talked in a while, I was wondering how are things going.\n";
 
 	std::cout << "Enter the name of another friend: \n";
-	std::string friend_name;
+	string friend_name;
 	std::cin >> friend_name;
 
-	std::cout << "Have you seen " << friend_name << " lately?";
+	std::cout << "Have you seen " << friend_name << " lately?\n";
 
+	std::cout << "Enter the age of the person you are writing to: \n";
+	int age{ -1 };
+	std::cin >> age;
+	if (age < 0 || age > 110)
+	{
+		error("you're kidding!");
+	}
 }
