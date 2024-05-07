65be5ee10679b69b604fa0b4725b0e815fad4eec
H0RIS <H0RIS@outlook.com>
2024-05-07 11:30:14 +0200

    Chapter3 Drill[2]

--- cpp/PPP/Chapter3/Drill/main.cpp
+++ cpp/PPP/Chapter3/Drill/main.cpp
@@ -12,9 +12,22 @@ int main()
 		nums.push_back(temp);
 		if (nums.size() == 2)
 		{
-		std::cout << "ints entered:\n" << nums[0] << "\t" << nums[1] << '\n';
-		nums = {};
-		std::cout << "Please enter 2 ints:\n";
+			std::cout << "ints entered:\n" << nums[0] << "\t" << nums[1] << '\n';
+
+			if (nums[0] > nums[1])
+			{
+				std::cout << "The smaller value is " << nums[1] << '\n';
+				std::cout << "The larger value is " << nums[0] << '\n';
+
+			}
+			else
+			{
+				std::cout << "The smaller value is " << nums[0] << '\n';
+				std::cout << "The larger value is " << nums[1] << '\n';
+			}
+
+			nums = {};
+			std::cout << "Please enter 2 ints:\n";
 		}
 	}
 }
