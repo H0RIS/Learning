e30f4baa7d47157f8a3dca788efffb7bc7e84007
H0RIS <H0RIS@outlook.com>
2024-05-13 10:17:37 +0200

    Chapter3 Drill[7]

--- cpp/PPP/Chapter3/Drill/main.cpp
+++ cpp/PPP/Chapter3/Drill/main.cpp
@@ -2,33 +2,55 @@ import std;
 
 constexpr double max_double{ std::numeric_limits<double>::max() };
 constexpr double lowest_double{ std::numeric_limits<double>::lowest() };
+std::string ask_message{ "Please enter a double followed by a unit( cm, in, ft, m ):" };
 
 int main()
 {
 	std::vector<double> nums;
-	double temp{ 0.0 };
+	double val{ 0.0 };
+	std::string unit{ "" };
 	double largest{ lowest_double };
 	double smallest{ max_double };
+	double conv_val{ 0.0 };
 
-	std::cout << "Please enter a double:\n";
+	std::cout << ask_message << '\n';
 
-	while (std::cin >> temp)
+	while (std::cin >> val >> unit)
 	{
-		nums.push_back(temp);
+		nums.push_back(val);
 
-		if (temp > largest)
+		if (unit == "m")
 		{
-			largest = temp;
+			conv_val = val;
+		}
+		else if (unit == "cm")
+		{
+			conv_val = val / 100;
+		}
+		else if (unit == "in")
+		{
+			conv_val = val * 2.54 / 100;
+		}
+		else if (unit == "ft")
+		{
+			conv_val = val * 2.54 * 12 / 100;
+		}
+
+		if (val > largest)
+		{
+			largest = val;
 			std::cout << "The largest so far\n";
 		}
 
-		if (temp < smallest)
+		if (val < smallest)
 		{
-			smallest = temp;
+			smallest = val;
 			std::cout << "The smallest so far\n";
 		}
 
-		std::cout << "Please enter a double:\n";
+		std::cout << "Entered value converted to m: " << conv_val << " m" << '\n';
+
+		std::cout << '\n' << ask_message << '\n';
 
 		/*
 		if (nums.size() == 2)
