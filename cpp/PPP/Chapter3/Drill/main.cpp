d4aa2b2ecbfea59f78a59449382c1fd277fa4ce2
H0RIS <H0RIS@outlook.com>
2024-05-13 10:30:19 +0200

    Chapter3 Drill[8]

--- cpp/PPP/Chapter3/Drill/main.cpp
+++ cpp/PPP/Chapter3/Drill/main.cpp
@@ -2,17 +2,17 @@ import std;
 
 constexpr double max_double{ std::numeric_limits<double>::max() };
 constexpr double lowest_double{ std::numeric_limits<double>::lowest() };
-std::string ask_message{ "Please enter a double followed by a unit( cm, in, ft, m ):" };
 
 int main()
 {
+	std::string ask_message{ "Please enter a double followed by a unit( cm, in, ft, m ):" };
 	std::vector<double> nums;
-	double val{ 0.0 };
+	double val{ lowest_double };
 	std::string unit{ "" };
 	double largest{ lowest_double };
 	double smallest{ max_double };
-	double conv_val{ 0.0 };
-
+	double conv_val{ lowest_double };
+	bool is_illegal{ false };
 	std::cout << ask_message << '\n';
 
 	while (std::cin >> val >> unit)
@@ -35,21 +35,29 @@ int main()
 		{
 			conv_val = val * 2.54 * 12 / 100;
 		}
-
-		if (val > largest)
+		else
 		{
-			largest = val;
-			std::cout << "The largest so far\n";
+			std::cout << "Illegal unit. Please choose from the the following! ( cm, in, ft, m )\n";
+			is_illegal = true;
 		}
 
-		if (val < smallest)
+		if (!is_illegal)
 		{
-			smallest = val;
-			std::cout << "The smallest so far\n";
+			if (val > largest)
+			{
+				largest = val;
+				std::cout << "The largest so far\n";
+			}
+
+			if (val < smallest)
+			{
+				smallest = val;
+				std::cout << "The smallest so far\n";
+			}
+
+			std::cout << "Entered value converted to m: " << conv_val << " m" << '\n';
 		}
-
-		std::cout << "Entered value converted to m: " << conv_val << " m" << '\n';
-
+		is_illegal = false;
 		std::cout << '\n' << ask_message << '\n';
 
 		/*
