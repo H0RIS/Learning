58b0c541d7682da3a39fa67c6fdfc1f0d2fdb122
H0RIS <H0RIS@outlook.com>
2024-05-29 13:42:06 +0200

    Chapter02 Exercises[09]

---
+++ Cpp/PPP/Chapter2/Exercises/09/main.cpp
@@ -0,0 +1,55 @@
+import std;
+
+int main()
+{
+	std::cout << "Enter a digit spelled out (\"two\"): ";
+	std::string value{};
+
+	while (std::cin >> value)
+	{
+		if (value == "zero")
+		{
+			std::cout << "You entered: 0\n";
+		}
+		else if (value == "one")
+		{
+			std::cout << "You entered: 1\n";
+		}
+		else if (value == "two")
+		{
+			std::cout << "You entered: 2\n";
+		}
+		else if (value == "three")
+		{
+			std::cout << "You entered: 3\n";
+		}
+		else if (value == "four")
+		{
+			std::cout << "You entered: 4\n";
+		}
+		else if (value == "five")
+		{
+			std::cout << "You entered: 5\n";
+		}
+		else if (value == "six")
+		{
+			std::cout << "You entered: 6\n";
+		}
+		else if (value == "seven")
+		{
+			std::cout << "You entered: 7\n";
+		}
+		else if (value == "eight")
+		{
+			std::cout << "You entered: 8\n";
+		}
+		else if (value == "nine")
+		{
+			std::cout << "You entered: 9\n";
+		}
+		else
+		{
+			std::cout << "Not a digit I know!\n";
+		}
+	}
+}
