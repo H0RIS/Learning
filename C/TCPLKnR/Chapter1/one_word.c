bc47d54681d2049ea8cc2fe16e13b1f662f3a6c7
H0RIS <H0RIS@outlook.com>
2024-07-09 19:15:08 +0200

    Exercise 1-12.

--- C/TCPLKnR/Chapter1/one_word.c
+++ C/TCPLKnR/Chapter1/one_word.c
@@ -1,30 +1,16 @@
 #include <stdio.h>
 
-#define IN  1
-#define OUT 1
-
 void main()
 {
     int c;
-    int state = IN;
 
     while ((c = getchar()) != EOF)
     {
         if (c == ' ' || c == '\n' || c == '\t')
         {
             printf("\n");
-            state = OUT;
             continue;
         }
-        else if(state == OUT)
-        {
-            state = IN;
-        }
-
-        if (state == IN)
-        {
-            putchar(c);
-        }
-        
+        putchar(c);
     }
 }
